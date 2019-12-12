9da06981ca596fc0e609f432bb3f9d58ad4dbe49
ablades <audaris@gmail.com>
2019-12-11 18:10:33 -0800

    add requests library

--- scraper.py
+++ scraper.py
@@ -6,19 +6,23 @@ import re
 import os
 from bs4 import BeautifulSoup
 
+def soupify():
+    with open('table.html', 'r') as f:
+            words = f.read()
 
-with open('table.html', 'r') as f:
-        words = f.read()
+    html = words
 
-html = words
+    soup = BeautifulSoup(html)
 
-soup = BeautifulSoup(html)
+    transcripts_urls = list()
 
-transcripts_urls = list()
+    #get all urls
+    for a in soup.find_all('a', href=True):
+        print ("Found the URL:", a['href'])
+        transcripts_urls.append(a['href'])
 
-#get all urls
-for a in soup.find_all('a', href=True):
-    print ("Found the URL:", a['href'])
-    transcripts_urls.append(a['href'])
+    return transcripts_urls
 
-print(transcripts_urls)
+
+if __name__ == "__main__":
+    print(soupify())
